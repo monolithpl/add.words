b2b0ad3c93de75f86a43ecc8eb445ee91a678255
Wiktor Jakubczyc <empikempik@gmail.com>
2017-10-09 11:37:23 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 var newWord = document.getElementById("word-new")
 var list = document.getElementById("word-list")
 var count = document.getElementById("word-count")
+var edit = document.querySelector('.edit')
 
+window.qsa = function (selector, scope) {
+	return (scope || document).querySelectorAll(selector);
+};
+	
+window.$on = function (target, type, callback, useCapture) {
+	target.addEventListener(type, callback, !!useCapture);
+};
+	
+window.$delegate = function (target, selector, type, handler) {
+	function dispatchEvent(event) {
+		var targetElement = event.target;
+		var potentialElements = window.qsa(selector, target);
+		var hasMatch = Array.prototype.indexOf.call(potentialElements, targetElement) >= 0;
+
+		if (hasMatch) {
+			handler.call(targetElement, event);
+		}
+	}
+	var useCapture = type === 'blur' || type === 'focus';
+	window.$on(target, type, dispatchEvent, useCapture);
+};
+	
 function updateCount(){
 	if (list.children.length > 1) count.innerHTML = list.children.length + ' words'
-	else if (list.children.length == 0) count.innerHTML = 'hit enter to add word'
+	else if (list.children.length == 0) count.innerHTML = 'type word and hit enter to add'
 	else count.innerHTML = list.children.length + ' word'
 }
 
@@ -34,6 +57,15 @@ document.addEventListener("click", function(event) {
 		}
     }
     event.stopPropagation()
+})
+
+$delegate(list, 'li .edit', 'keyup', function (event) {
+	if (event.keyCode == 13 || event.keyCode == 27)
+	{
+		var element = document.querySelector('.edit')
+		var text = element.value
+		element.parentNode.innerHTML = text
+	}
 })
 
 newWord.addEventListener("keypress", function(event) {

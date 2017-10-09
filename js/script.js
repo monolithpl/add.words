6ca03be7fc79bad6a86653f9535567533be16c47
Wiktor Jakubczyc <empikempik@gmail.com>
2017-10-09 03:53:36 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-var newWord = document.getElementById("word-new");
-var list = document.getElementById("word-list");
-var count = document.getElementById("word-count");
+var newWord = document.getElementById("word-new")
+var list = document.getElementById("word-list")
+var count = document.getElementById("word-count")
 
 function updateCount(){
 	if (list.children.length > 1) count.innerHTML = list.children.length + ' words'
@@ -8,24 +8,41 @@ function updateCount(){
 	else count.innerHTML = list.children.length + ' word'
 }
 
-list.addEventListener("click", function(event) {
+document.addEventListener("click", function(event) {
     if (event.target !== event.currentTarget) {
         if (event.target.className == "destroy")
 		{
-			event.target.parentElement.parentNode.removeChild(event.target.parentElement);
-			updateCount();
+			event.target.parentElement.parentNode.removeChild(event.target.parentElement)
+			updateCount()
+		}
+		if (event.target.className !== "edit" && document.querySelector('.edit'))
+		{
+			var element = document.querySelector('.edit')
+			var text = element.value
+			element.parentNode.innerHTML = text
+		}
+		if (event.target.className == "listitem")
+		{
+			if (document.querySelector('.edit')) return
+			var text = event.target.innerHTML
+			var input = document.createElement('input')
+			input.className = 'edit'
+			event.target.appendChild(input)
+			input.value = text
+			input.focus()
+			input.setSelectionRange(0, input.value.length)
 		}
     }
-    event.stopPropagation();
-});
+    event.stopPropagation()
+})
 
 newWord.addEventListener("keypress", function(event) {
     if (event.keyCode == 13 && newWord.value.trim() !== '')
 	{
-		var entry = document.createElement('li');
-		entry.innerHTML = '<label>' + newWord.value + '</label><button class="destroy"></button>';
-		list.insertBefore(entry, list.childNodes[0]);
-		newWord.value = '';
-		updateCount();
+		var entry = document.createElement('li')
+		entry.innerHTML = '<label class="listitem">' + newWord.value + '</label><button class="destroy"></button>'
+		list.insertBefore(entry, list.childNodes[0])
+		newWord.value = ''
+		updateCount()
 	}
-});
+})
